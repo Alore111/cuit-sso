2f419dabb17a04fb9016a250c6617a36d8387bec
Allllore <2050791391@qq.com>
2024-06-20 05:46:02 +0800

    feat:新增ddddocr识别验证码，手动输入验证码，封装类

---
+++ README.md
@@ -0,0 +1,176 @@
+# CUIT-SSO
+
+### 项目简介
+
+本项目旨在自动化CUIT统一身份认证系统的登录过程，包括验证码识别。项目支持多种验证码识别方式，并使用Python模拟登录流程。
+
+### 功能特点
+
+- 管理Cookie以维持会话。
+- 支持多种验证码识别方法：
+  - **超级鹰（Chaojiying）**：在线验证码识别服务。
+  - **ddddocr**：开源OCR库用于验证码识别。
+  - **手动输入**：如果自动识别失败，保存验证码图片供手动输入。
+- 使用 `loguru` 进行日志管理。
+
+### 开发计划
+
+- [x] 支持手动输入验证码。
+- [x] 支持使用超级鹰识别验证码。
+- [x] 支持使用ddddocr识别验证码。
+- [x] 支持使用Cookie管理会话。
+- [ ] 支持WebVPN登录。
+- [ ] 支持使用代理IP。
+
+### 环境要求
+
+- Python 3.10+
+- 所需库：`requests`, `configparser`, `loguru`, `ddddocr`
+
+### 安装步骤
+
+1. **克隆项目仓库：**
+   ```sh
+   git clone https://github.com/Alore111/cuit-sso.git
+   cd cuit-sso
+   ```
+
+2. **创建并激活虚拟环境（venv）：**
+   ```sh
+   python -m venv venv
+   source venv/bin/activate  # 对于Windows用户，使用 `venv\Scripts\activate`
+   ```
+
+3. **安装依赖：**
+   ```sh
+   pip install -r requirements.txt
+   ```
+
+4. **配置设置：**
+
+   编辑 `config.ini` 文件以设置账户信息和服务URL。文件格式如下：
+
+   ```ini
+    # 校园统一身份认证账号密码
+    [ACCOUNT]
+    username = your_username
+    password = your_password
+
+    # 验证码识别方式
+    # 1: 超级鹰在线识别（需注册超级鹰账号并获取题分）
+    # 2: 开源学习库本地识别（ddddocr第三方库）
+    # 3：手动输入（验证码图片位于captcha.jpg）
+    [CAPTCHA]
+    crack_type = 2
+
+    # 超级鹰账号密码
+    # 需注册超级鹰账号并获取题分
+    # https://www.chaojiying.com/
+    [CHAOJIYING]
+    username = your_cjy_username
+    password = your_cjy_password
+    soft_id = your_cjy_soft_id
+
+    [URLS]
+    login_url = https://sso.cuit.edu.cn/authserver/login
+    data_list_url = https://sso.cuit.edu.cn/authserver/data/list
+    captcha_url = https://sso.cuit.edu.cn/authserver/captcha
+    login_check_url = https://sso.cuit.edu.cn/authserver/loginCheck
+    service_url = http://jwgl.cuit.edu.cn/eams/login.action
+    act_elect_course = http://jwgl.cuit.edu.cn/eams/stdElectCourse.action
+   ```
+
+### 使用方法
+
+1. **激活虚拟环境：**
+   ```sh
+   source venv/bin/activate  # 对于Windows用户，使用 `venv\Scripts\activate`
+   ```
+
+2. **运行脚本：**
+   ```sh
+   python main.py
+   ```
+
+3. **手动输入验证码（如果配置）：**
+   - 如果选择手动输入验证码（crack_type = 3），验证码图片将保存到项目目录中的 `captcha.jpg` 文件。你需要查看该图片并在提示时手动输入验证码。
+
+### 代码概览
+
+- **CUITSSO 类：** 包含处理登录过程的所有方法，包括获取Cookie、处理验证码和提交登录表单。
+- **main.py：** 初始化登录过程并配置日志。
+
+### 方法概述
+
+- `get_cookie()`: 获取初始会话Cookie。
+- `get_data_list()`: 使用会话Cookie获取数据列表。
+- `get_captcha(timestamp)`: 获取验证码图片。
+- `login_check(captcha)`: 使用提供的验证码检查登录状态。
+- `get_ticket(execution, captcha)`: 成功验证码验证后获取登录票据。
+- `get_jsession()`: 获取 `jsession` Cookie。
+- `get_gsession()`: 获取 `gsession` Cookie。
+- `login()`: 执行登录过程的主函数。
+
+### 日志管理
+
+日志将保存到 `logfile.log` 文件中，包括过程中的详细信息。
+
+### 验证码处理
+
+脚本支持三种验证码识别方式：
+
+1. **超级鹰（crack_type = 1）**
+2. **ddddocr（crack_type = 2）**
+3. **手动输入（crack_type = 3）**
+
+修改 `config.ini` 中的 `crack_type` 值以选择所需的识别方法。
+
+### 配置示例 (`config.ini`)
+
+```ini
+# 校园统一身份认证账号密码
+[ACCOUNT]
+username = your_username
+password = your_password
+
+# 验证码识别方式
+# 1: 超级鹰在线识别（需注册超级鹰账号并获取题分）
+# 2: 开源学习库本地识别（ddddocr第三方库）
+# 3：手动输入（验证码图片位于captcha.jpg）
+[CAPTCHA]
+crack_type = 2
+
+# 超级鹰账号密码
+# 需注册超级鹰账号并获取题分
+# https://www.chaojiying.com/
+[CHAOJIYING]
+username = your_cjy_username
+password = your_cjy_password
+soft_id = your_cjy_soft_id
+
+[URLS]
+login_url = https://sso.cuit.edu.cn/authserver/login
+data_list_url = https://sso.cuit.edu.cn/authserver/data/list
+captcha_url = https://sso.cuit.edu.cn/authserver/captcha
+login_check_url = https://sso.cuit.edu.cn/authserver/loginCheck
+service_url = http://jwgl.cuit.edu.cn/eams/login.action
+act_elect_course = http://jwgl.cuit.edu.cn/eams/stdElectCourse.action
+```
+
+### 注意事项
+
+- 如果使用超级鹰进行验证码识别，请确保拥有有效的超级鹰账户。
+- ddddocr库可能占用较多内存。
+- 手动输入需要用户交互来输入验证码。
+
+### 贡献
+
+欢迎Fork此仓库，进行改进并提交Pull Request。非常欢迎各类贡献！
+
+### 许可证
+
+此项目采用GPL许可证。详情请参阅LICENSE文件。
+
+### 联系方式
+
+如有任何问题或需要帮助，请在GitHub上提出Issue或联系 [alore@2ndtool.top]。
--- main.py
+++ main.py
@@ -1,203 +1,287 @@
+import contextlib
+import io
 import requests
 import configparser
 import time
 from loguru import logger
 import chaojiying as cjy
 from urllib.parse import urlparse, parse_qs
+import ddddocr
 
 # 配置 loguru 日志格式
 logger.add("logfile.log", format="{time} | {level} | {message}", level="DEBUG")
 
-# 读取配置文件
-config = configparser.ConfigParser()
-config.read('config.ini')
+class CUITSSO:
+    def __init__(self, username, password):
+        # 读取配置文件
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini', encoding='utf-8')
 
-# 从配置文件中获取信息
-ACCOUNT = config['ACCOUNT']
-CJY = config['CHAOJIYING']
-URLS = config['URLS']
+        # 从配置文件中获取信息
+        self.ACCOUNT = self.config['ACCOUNT']
+        self.CJY = self.config['CHAOJIYING']
+        self.CAPTCHA = self.config['CAPTCHA']
+        self.URLS = self.config['URLS']
 
-def get_cookie():
-    url = URLS['login_url']
-    params = {"service": URLS['service_url']}
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-        "Accept-Encoding": "gzip, deflate",
-        "Upgrade-Insecure-Requests": "1",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-User": "?1",
-        "Te": "trailers"
-    }
+        self.username = username
+        self.password = password
+        self.crack_type = int(self.CAPTCHA['crack_type'])
+        self.cookie = None
+        self.ticket = None
+        self.tgc = None
+        self.jsession = None
+        self.gsession = None
 
-    try:
-        response = requests.post(url, params=params, headers=headers)
-        response.raise_for_status()
-        cookies = response.cookies.get_dict()
-        return {
-            "route": cookies.get('route'),
-            "SESSION": cookies.get('SESSION')
+        if self.crack_type == 1:
+            self.chaojiying = cjy.Chaojiying_Client(self.CJY['username'], self.CJY['password'], self.CJY['soft_id'])
+
+    def get_cookie(self):
+        url = self.URLS['login_url']
+        params = {"service": self.URLS['service_url']}
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+            "Accept-Encoding": "gzip, deflate",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Te": "trailers"
         }
-    except requests.RequestException as e:
-        logger.error(f"获取Cookie失败: {e}")
-        return None
 
-def get_data_list(cookie):
-    url = URLS['data_list_url']
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-        "Accept-Encoding": "gzip, deflate",
-        "Cookie": f"route={cookie['route']}; SESSION={cookie['SESSION']}",
-    }
+        try:
+            response = requests.post(url, params=params, headers=headers)
+            response.raise_for_status()
+            cookies = response.cookies.get_dict()
+            self.cookie = {
+                "route": cookies.get('route'),
+                "SESSION": cookies.get('SESSION')
+            }
+        except requests.RequestException as e:
+            logger.error(f"获取Cookie失败: {e}")
+            self.cookie = None
 
-    try:
-        response = requests.post(url, headers=headers)
-        response.raise_for_status()
-        return response.text
-    except requests.RequestException as e:
-        logger.error(f"获取数据列表失败: {e}")
-        return None
+    def get_data_list(self):
+        url = self.URLS['data_list_url']
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+            "Accept-Encoding": "gzip, deflate",
+            "Cookie": f"route={self.cookie['route']}; SESSION={self.cookie['SESSION']}",
+        }
 
-def get_captcha(timestamp, cookie):
-    url = URLS['captcha_url']
-    params = {"timestamp": timestamp}
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-        "Accept-Encoding": "gzip, deflate",
-        "Cookie": f"route={cookie['route']}; SESSION={cookie['SESSION']}",
-        "Upgrade-Insecure-Requests": "1",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-User": "?1",
-        "Te": "trailers"
-    }
+        try:
+            response = requests.post(url, headers=headers)
+            response.raise_for_status()
+            return response.text
+        except requests.RequestException as e:
+            logger.error(f"获取数据列表失败: {e}")
+            return None
 
-    try:
-        response = requests.get(url, params=params, headers=headers)
-        response.raise_for_status()
-        return response
-    except requests.RequestException as e:
-        logger.error(f"获取验证码失败: {e}")
-        return None
+    def get_captcha(self, timestamp):
+        url = self.URLS['captcha_url']
+        params = {"timestamp": timestamp}
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+            "Accept-Encoding": "gzip, deflate",
+            "Cookie": f"route={self.cookie['route']}; SESSION={self.cookie['SESSION']}",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Te": "trailers"
+        }
 
-def loginCheck(username, password, captcha, cookie):
-    url = URLS['login_check_url']
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-        "Accept-Encoding": "gzip, deflate",
-        "Cookie": f"route={cookie['route']}; SESSION={cookie['SESSION']}",
-        "Upgrade-Insecure-Requests": "1",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-User": "?1",
-        "Te": "trailers"
-    }
-    params = {
-        'name': username,
-        'pwd': password,
-        'captcha': captcha
-    }
+        try:
+            response = requests.get(url, params=params, headers=headers)
+            response.raise_for_status()
+            return response
+        except requests.RequestException as e:
+            logger.error(f"获取验证码失败: {e}")
+            return None
 
-    try:
-        response = requests.get(url, headers=headers, params=params)
-        response.raise_for_status()
-        return response.text == 'success'
-    except requests.RequestException as e:
-        logger.error(f"登录检查失败: {e}")
-        return False
+    def login_check(self, captcha):
+        url = self.URLS['login_check_url']
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+            "Accept-Encoding": "gzip, deflate",
+            "Cookie": f"route={self.cookie['route']}; SESSION={self.cookie['SESSION']}",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Te": "trailers"
+        }
+        params = {
+            'name': self.username,
+            'pwd': self.password,
+            'captcha': captcha
+        }
 
-def get_ticket(execution, username, password, captcha, cookie):
-    url = f"{URLS['login_url']}?service={URLS['service_url']}"
-    headers = {
-        'Host': 'sso.cuit.edu.cn',
-        "Cookie": f"route={cookie['route']}; SESSION={cookie['SESSION']}",
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-        'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
-        'Accept-Encoding': 'gzip, deflate',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Origin': 'https://sso.cuit.edu.cn',
-        'Referer': f"https://sso.cuit.edu.cn/authserver/login?service={URLS['service_url']}",
-        'Upgrade-Insecure-Requests': '1',
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'same-origin',
-        'Sec-Fetch-User': '?1',
-        'Te': 'trailers',
-        'Connection': 'close'
-    }
-    data = {
-        'execution': execution,
-        '_eventId': 'submit',
-        'lm': 'usernameLogin',
-        'geolocation': '',
-        'username': username,
-        'password': password,
-        'captcha': captcha
-    }
+        try:
+            response = requests.get(url, headers=headers, params=params)
+            response.raise_for_status()
+            return response.text == 'success'
+        except requests.RequestException as e:
+            logger.error(f"登录检查失败: {e}")
+            return False
 
-    try:
-        response = requests.post(url, headers=headers, data=data, allow_redirects=False)
-        response.raise_for_status()
-        redirect_location = response.headers.get('Location')
-        set_cookie = response.headers.get('Set-Cookie')
-        if redirect_location:
-            parsed_url = urlparse(redirect_location)
-            ticket = parse_qs(parsed_url.query).get('ticket', [None])[0]
-            cookieTGC = set_cookie.split(';')[0].split('=')[1]
-            return ticket, cookieTGC
+    def get_ticket(self, execution, captcha):
+        url = f"{self.URLS['login_url']}?service={self.URLS['service_url']}"
+        headers = {
+            'Host': 'sso.cuit.edu.cn',
+            "Cookie": f"route={self.cookie['route']}; SESSION={self.cookie['SESSION']}",
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+            'Accept-Encoding': 'gzip, deflate',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Origin': 'https://sso.cuit.edu.cn',
+            'Referer': f"https://sso.cuit.edu.cn/authserver/login?service={self.URLS['service_url']}",
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-User': '?1',
+            'Te': 'trailers',
+            'Connection': 'close'
+        }
+        data = {
+            'execution': execution,
+            '_eventId': 'submit',
+            'lm': 'usernameLogin',
+            'geolocation': '',
+            'username': self.username,
+            'password': self.password,
+            'captcha': captcha
+        }
+
+        try:
+            response = requests.post(url, headers=headers, data=data, allow_redirects=False)
+            response.raise_for_status()
+            redirect_location = response.headers.get('Location')
+            set_cookie = response.headers.get('Set-Cookie')
+            if redirect_location:
+                parsed_url = urlparse(redirect_location)
+                ticket = parse_qs(parsed_url.query).get('ticket', [None])[0]
+                self.ticket = ticket
+                self.tgc = set_cookie.split(';')[0].split('=')[1]
+            else:
+                logger.error("重定向地址中没有找到ticket")
+        except requests.RequestException as e:
+            logger.error(f"获取票据失败: {e}")
+            self.ticket = None
+            self.tgc = None
+
+    def get_jsession(self):
+        url = f"{self.URLS['act_elect_course']}"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+            "Accept-Encoding": "gzip, deflate",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Te": "trailers"
+        }
+
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            self.jsession = response.headers.get('Set-Cookie').split(';')[0].split('=')[1]
+        except requests.RequestException as e:
+            logger.error(f"获取jsession失败: {e}")
+            self.jsession = None
+
+    def get_gsession(self):
+        url = f"{self.URLS['act_elect_course']}"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+            "Accept-Encoding": "gzip, deflate",
+            "Connection": "keep-alive",
+            "Cookie": "JSESSIONID=" + self.jsession,
+            "Upgrade-Insecure-Requests": "1",
+        }
+
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            self.gsession = response.headers.get('Set-Cookie').split(';')[0].split('=')[1]
+        except requests.RequestException as e:
+            logger.error(f"获取gsession失败: {e}")
+            self.gsession = None
+
+    def login(self):
+        self.get_cookie()
+        if self.cookie:
+            for attempt in range(3):
+                timestamp = str(int(time.time() * 1000))
+                cap = self.get_captcha(timestamp)
+                if cap:
+                    with open("captcha.jpg", "wb") as f:
+                        f.write(cap.content)
+                    img = cap.content
+                    if self.crack_type == 1:
+                        captcha_str = self.chaojiying.PostPic(img, 1004)["pic_str"]
+                        logger.info(f"验证码识别结果：{captcha_str}")
+                    elif self.crack_type == 2:
+                        # 初始化 ddddocr 对象，抑制输出消息
+                        with contextlib.redirect_stdout(io.StringIO()), contextlib.redirect_stderr(io.StringIO()):
+                            ocr = ddddocr.DdddOcr()
+                            captcha_str = ocr.classification(img)
+                            logger.info(f"验证码识别结果：{captcha_str}")
+                    else:
+                        captcha_str = input("请输入验证码(图片位于captcha.jpg)：")
+                    
+                    is_login = self.login_check(captcha_str)
+                    # logger.info(f"登录结果：{is_login}")
+                    if is_login:
+                        self.get_ticket('e1s1', captcha_str)
+                        self.get_jsession()
+                        if self.jsession:
+                            self.get_gsession()
+                        if self.ticket and self.tgc:
+                            logger.success("登录成功")
+                            break
+                        else:
+                            logger.error("获取ticket或cookie失败")
+                    else:
+                        logger.error(f"登录失败，第{attempt + 1}次重试")
+                else:
+                    logger.error("获取验证码失败")
+            else:
+                logger.error("登录失败次数达到上限")
         else:
-            logger.error("重定向地址中没有找到ticket")
-            return None, None
-    except requests.RequestException as e:
-        logger.error(f"获取票据失败: {e}")
-        return None, None
+            logger.error("获取cookie失败")
 
 
 if __name__ == "__main__":
-    chaojiying = cjy.Chaojiying_Client(CJY['username'], CJY['password'], CJY['soft_id'])
-    cookie = get_cookie()
-    if cookie:
-        logger.info(f"获取到cookie：{cookie}")
-        dataList = get_data_list(cookie)
-        logger.info(f"获取到数据")
-        for attempt in range(3):
-            timestamp = str(int(time.time() * 1000))
-            cap = get_captcha(timestamp, cookie)
-            if cap:
-                with open("captcha.jpg", "wb") as f:
-                    f.write(cap.content)
-                img = cap.content
-                captcha_str = chaojiying.PostPic(img, 1004)["pic_str"]
-                logger.info(f"验证码识别结果：{captcha_str}")
-                isLogin = loginCheck(ACCOUNT['username'], ACCOUNT['password'], captcha_str, cookie)
-                logger.info(f"登录结果：{isLogin}")
-                if isLogin:
-                    ticket, tgc = get_ticket('e1s1', ACCOUNT['username'], ACCOUNT['password'], captcha_str, cookie)
-                    if ticket and tgc:
-                        logger.info(f"获取到ticket：{ticket}")
-                        logger.info(f"获取到cookieTGC：{tgc}")
-                        break
-                    else:
-                        logger.error("获取ticket或cookie失败")
-                else:
-                    logger.error(f"登录失败，第{attempt + 1}次重试")
-            else:
-                logger.error("获取验证码失败")
-        else:
-            logger.error("登录失败次数达到上限")
-    else:
-        logger.error("获取cookie失败")
+    # 读取配置文件
+    config = configparser.ConfigParser()
+    config.read('config.ini', encoding='utf-8')
 
+    # 从配置文件中获取信息
+    ACCOUNT = config['ACCOUNT']
 
+    cuit_login = CUITSSO(ACCOUNT['username'], ACCOUNT['password'])
+    cuit_login.login()
+    
+    logger.info(f"gsession: {cuit_login.gsession}")
+    logger.info(f"jsession: {cuit_login.jsession}")
+    logger.info(f"ticket: {cuit_login.ticket}")
+    logger.info(f"tgc: {cuit_login.tgc}")
+    logger.info(f"cookie: {cuit_login.cookie}")
